0d1c8c5bfb64e86a3681abc6988cdd709c89b6e6
Jack Neil <jack.neil@udifi.com>
2019-07-08 13:37:34 -0400

    tweaks for azure

--- webserver.py
+++ webserver.py
@@ -45,5 +45,5 @@ def candidates():
 
 
 
-if __name__ == '__main__':
-    MSC.run(debug=True, host='0.0.0.0', port=80)
+#if __name__ == '__main__':
+#    MSC.run(debug=True, host='0.0.0.0', port=80)
