2feac9d8d47c1e1b500f47044733b722308fd74c
Jack Neil <jack.neil@udifi.com>
2019-07-08 14:54:59 -0400

    improved responses

--- webserver.py
+++ webserver.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 import jamspell, json
+from json2html import *
+
 corrector = jamspell.TSpellCorrector()
 corrector.LoadLangModel('model_medical.bin')
 
@@ -12,20 +14,29 @@ def hello():
 @MSC.route("/fix")
 def fix():
     text = request.args.get('text', default = "", type = str)
+    htmlflag = request.args.get('html', default=False, type = bool)
+
     if text == "":
-        return "No text received. Usage: url/fix?text=texttomedicalspellcheck"
-    return corrector.FixFragment(text)
+        return "No text received. Usage: url/fix?html=0&text=texttomedicalspellcheck"
+    rval = {}
+    rval['input']= text
+    rval['results'] = corrector.FixFragment(text)
+    if htmlflag: return json2html.convert(json.dumps(rval))
+    else: return json.dumps(rval)
 
 @MSC.route("/candidates")
 def candidates():
     text = request.args.get('text', default = "", type = str)
     limit = request.args.get('limit', default = 5, type = int)
+    htmlflag = request.args.get('html', default=False, type = bool)  
+
     rval = {}
+    rval['input'] = text
     runningOffset=0
     print(text)
     print(text.split())
     if text == "":
-        return "No text received. Usage: url/candidates?text=texttomedicalspellcheck"
+        return "No text received. Usage: url/candidates?html=0&limit=2&text=texttomedicalspellcheck"
     rval['results'] = []
     words = text.split()
     index = text.index
@@ -34,16 +45,17 @@ def candidates():
         runningOffset = wordOffset + len(word)
         cands = corrector.GetCandidates(words, i)
         if len(cands) == 0: continue
-        if cands[0] == word: continue
+        if cands[0].lower() == word.lower(): continue
         candict = {'candidates': cands[:limit],
         'len': len(word),
         'pos_from': wordOffset
         }
         rval['results'].append(candict)
     print(rval)
-    return json.dumps(rval,indent=2)
+    if htmlflag: return json2html.convert(json.dumps(rval))
+    else: json.dumps(rval,indent=2)
 
 
 
-#if __name__ == '__main__':
-#    MSC.run(debug=True, host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    MSC.run(debug=True, host='0.0.0.0', port=80)
