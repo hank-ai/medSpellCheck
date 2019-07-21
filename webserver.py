d585bd570e9976244ab34a17fc5c25d88e2628ba
Jack Neil <jack.neil@udifi.com>
2019-07-21 07:39:22 -0400

    pointing to new medical model

--- webserver.py
+++ webserver.py
@@ -5,8 +5,8 @@ from json2html import *
 
 print('Starting up ...')
 corrector = jamspell.TSpellCorrector()
-print('Loading medical model ...')
-corrector.LoadLangModel('c:/_aidata/medSpellCheck/model_medical.v1.bin')
+#print('Loading medical model ...')
+corrector.LoadLangModel('c:/_aidata/medSpellCheck/model_medical.v2.bin')
 
 MSC = Flask(__name__) #medSpellCheck
 
